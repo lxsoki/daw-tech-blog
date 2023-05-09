a26841b22a2d14490a00557d5259121395758f02
Dora <dora94558@gmail.com>
2023-05-09 19:03:20 +0300

    xfb

--- carousel.php
+++ carousel.php
@@ -1,20 +1,28 @@
 <div class="carousel relative overflow-hidden mb-5 -mt-2">
-    <div class="carousel-inner">
-        <!-- <a href="cat-page4.php#cat4-post1"> -->
-            <div class="carousel-item w-full h-64 md:h-96 bg-cover bg-center active" style="background-image: url('assets/wwdc23-682x384.jpg'); background-size: contain; background-repeat: no-repeat; background-color:black;"></div>
-        <!-- </a> -->
-        <div class="carousel-item w-full h-64 md:h-96 bg-cover bg-center" style="background-image: url('assets/Apple-Music-Classical-hero.jpg'); background-size: contain; background-repeat: no-repeat;background-color:black;"></div>
-        <div class="carousel-item w-full h-64 md:h-96 bg-cover bg-center" style="background-image: url('assets/macOS+Ventura+Compatibility+hero.jpg'); background-size: contain; background-repeat: no-repeat;background-color:black;"></div>
+  <div class="carousel-inner">
+    <!-- <a href="cat-page4.php#cat4-post1"> -->
+    <div class="carousel-item w-full h-64 md:h-96 bg-cover bg-center active"
+      style="background-image: url('assets/Expect\ problems\ and\ eat\ them\ for\ breakfast.png'); background-size: contain; background-repeat: no-repeat; background-color:black;">
     </div>
-    <button class="carousel-arrow carousel-arrow-left">
-        <span class="iconify" data-icon="mdi:chevron-left" data-inline="false" data-width="24" data-height="24" data-color="white"></span>
-    </button>
-    <button class="carousel-arrow carousel-arrow-right">
-        <span class="iconify" data-icon="mdi:chevron-right" data-inline="false" data-width="24" data-height="24" data-color="white"></span>
-    </button>
-    <div class="carousel-indicators absolute bottom-0 mb-4 w-full">
-        <button class="carousel-indicator active"></button>
-        <button class="carousel-indicator"></button>
-        <button class="carousel-indicator"></button>
+    <!-- </a> -->
+    <div class="carousel-item w-full h-64 md:h-96 bg-cover bg-center"
+      style="background-image: url('assets/back_to_work-wallpaper-960x640.jpg'); background-size: contain; background-repeat: no-repeat;background-color:black;">
     </div>
+    <div class="carousel-item w-full h-64 md:h-96 bg-cover bg-center"
+      style="background-image: url('assets/analysis-640x360.jpg'); background-size: contain; background-repeat: no-repeat;background-color:black;">
+    </div>
+  </div>
+  <button class="carousel-arrow carousel-arrow-left">
+    <span class="iconify" data-icon="mdi:chevron-left" data-inline="false" data-width="24" data-height="24"
+      data-color="white"></span>
+  </button>
+  <button class="carousel-arrow carousel-arrow-right">
+    <span class="iconify" data-icon="mdi:chevron-right" data-inline="false" data-width="24" data-height="24"
+      data-color="white"></span>
+  </button>
+  <div class="carousel-indicators absolute bottom-0 mb-4 w-full">
+    <button class="carousel-indicator active"></button>
+    <button class="carousel-indicator"></button>
+    <button class="carousel-indicator"></button>
+  </div>
 </div>
--- footer.php
+++ footer.php
@@ -1,25 +1,26 @@
 <footer class="bg-gray-800 shadow-md">
-    <div class="container mx-auto px-6 py-8">
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
-            <div>
-                <h3 class="text-xl font-semibold mb-4">Follow Us</h3>
-                <ul class="flex space-x-4 text-gray-300">
-                    <li>
-                        <a href="https://github.com/DoraMurgescu/" class="hover:text-white" target="_blank">
-                            <span class="iconify" data-icon="mdi:github" data-inline="false" data-width="24" data-height="24"></span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/lxsoki/" class="hover:text-white" target="_blank">
-                            <span class="iconify" data-icon="mdi:github" data-inline="false" data-width="24" data-height="24"></span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <h3 class="text-xl font-semibold mb-4">Copyright</h3>
-                <p class="text-gray-300">&copy; 2023 Dora Murgescu & Soki Alex. All rights reserved.</p>
-            </div>
-        </div>
+  <div class="container mx-auto px-6 py-8">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div>
+        <h3 class="text-xl font-semibold mb-4">Follow Us</h3>
+        <ul class="flex space-x-4 text-gray-300">
+          <li>
+            <a href="https://github.com/DoraMurgescu/" class="hover:text-white" target="_blank">
+              <span class="iconify" data-icon="mdi:github" data-inline="false" data-width="24" data-height="24"></span>
+            </a>
+          </li>
+          <p>&</p>
+          <li>
+            <a href="https://github.com/lxsoki/" class="hover:text-white" target="_blank">
+              <span class="iconify" data-icon="mdi:github" data-inline="false" data-width="24" data-height="24"></span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3 class="text-xl font-semibold mb-4">Copyright</h3>
+        <p class="text-gray-300">&copy; 2023 Dora Murgescu & Soki Alex. All rights reserved.</p>
+      </div>
     </div>
+  </div>
 </footer>
