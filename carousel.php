6155279fac21fe48fa2d2532fb533a3d3bf896fb
lxsoki <soki.alx@gmail.com>
2023-05-10 22:28:02 +0300

    added favicon.ico file, removed obsolete files (cat-pages)

--- carousel.php
+++ carousel.php
@@ -1,24 +1,17 @@
 <div class="carousel relative overflow-hidden mb-5 -mt-2">
   <div class="carousel-inner">
-    <!-- <a href="cat-page4.php#cat4-post1"> -->
-    <div class="carousel-item w-full h-64 md:h-96 bg-cover bg-center active"
-      style="background-image: url('assets/Expect\ problems\ and\ eat\ them\ for\ breakfast.png'); background-size: contain; background-repeat: no-repeat; background-color:black;">
+    <div class="carousel-item w-full h-64 md:h-96 bg-cover bg-center active" style="background-image: url('assets/Expect\ problems\ and\ eat\ them\ for\ breakfast.png'); background-size: contain; background-repeat: no-repeat; background-color:black;">
     </div>
-    <!-- </a> -->
-    <div class="carousel-item w-full h-64 md:h-96 bg-cover bg-center"
-      style="background-image: url('assets/back_to_work-wallpaper-960x640.jpg'); background-size: contain; background-repeat: no-repeat;background-color:black;">
+    <div class="carousel-item w-full h-64 md:h-96 bg-cover bg-center" style="background-image: url('assets/back_to_work-wallpaper-960x640.jpg'); background-size: contain; background-repeat: no-repeat;background-color:black;">
     </div>
-    <div class="carousel-item w-full h-64 md:h-96 bg-cover bg-center"
-      style="background-image: url('assets/analysis-640x360.jpg'); background-size: contain; background-repeat: no-repeat;background-color:black;">
+    <div class="carousel-item w-full h-64 md:h-96 bg-cover bg-center" style="background-image: url('assets/analysis-640x360.jpg'); background-size: contain; background-repeat: no-repeat;background-color:black;">
     </div>
   </div>
   <button class="carousel-arrow carousel-arrow-left">
-    <span class="iconify" data-icon="mdi:chevron-left" data-inline="false" data-width="24" data-height="24"
-      data-color="white"></span>
+    <span class="iconify" data-icon="mdi:chevron-left" data-inline="false" data-width="24" data-height="24" data-color="white"></span>
   </button>
   <button class="carousel-arrow carousel-arrow-right">
-    <span class="iconify" data-icon="mdi:chevron-right" data-inline="false" data-width="24" data-height="24"
-      data-color="white"></span>
+    <span class="iconify" data-icon="mdi:chevron-right" data-inline="false" data-width="24" data-height="24" data-color="white"></span>
   </button>
   <div class="carousel-indicators absolute bottom-0 mb-4 w-full">
     <button class="carousel-indicator active"></button>
